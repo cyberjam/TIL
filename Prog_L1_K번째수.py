56b81d6187cecda64cad6aa2a839fff2d49ca783
Jae Min Kim <jamsince1994@naver.com>
2020-06-29 17:55:03 +0900

    Update Prog_L1_K번째수.py

--- Prog_L1_K번째수.py
+++ Prog_L1_K번째수.py
@@ -1,3 +1,5 @@
 #K번째수
 def solution(array, commands):
-    return [sorted(array[i[0]-1:i[1]])[i[2]-1] for i in commands]  #i는 중첩리스트 내부 리스트, sorted는 정렬된 결과를 반환      
+    return [sorted(array[i[0]-1:i[1]])[i[2]-1] for i in commands]  #i는 중첩리스트 내부 리스트, sorted는 정렬된 결과를 반환, [i[2]-1]는 정렬후 몇번째
+# input 형식
+# [2,5,1] 2번째에서 5번째까지 잘라 정렬후 1번째
