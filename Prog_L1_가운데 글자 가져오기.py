ecfa512321970ebb7fe09b8028cd0fb8f2283228
Jae Min Kim <jamsince1994@naver.com>
2020-07-01 17:42:26 +0900

    Update Prog_L1_가운데 글자 가져오기.py

--- Prog_L1_가운데 글자 가져오기.py
+++ Prog_L1_가운데 글자 가져오기.py
@@ -8,3 +8,8 @@ def solution(s):
     else:
         answer=s[q-1:q+1]
     return answer
+
+#다른사람 풀이
+
+def solution(s):
+    return s[(len(s)-1)//2:len(s)//2+1] # //가 -,+ 보다 우선순위가 높다. 길이가 홀수든 짝수든 가능 # 변수 할당없이 대단
