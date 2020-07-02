ec6782a72e7f5d7d436884baee4097b976c8f62f
Jae Min Kim <jamsince1994@naver.com>
2020-07-02 11:22:13 +0900

    Update Prog_L1_수박수박수.py

--- Prog_L1_수박수박수.py
+++ Prog_L1_수박수박수.py
@@ -32,4 +32,11 @@ def water_melon(n):
 print("n이 3인 경우: " + water_melon(3));
 print("n이 4인 경우: " + water_melon(4));
 
-#이게 더 효율적이고 참신하다
+#이게 더 효율적
+
+
+#친구 풀이3
+# https://github.com/cyberjam/Programmers/blob/master/%EB%82%B4%EA%B0%80%20%ED%92%80%EC%96%B4%EB%B3%B8%20%EB%8B%B5%EC%95%88/%EC%88%98%EB%B0%95%EC%88%98%EB%B0%95%EC%88%98%EB%B0%95%EC%88%98%EB%B0%95%EC%88%98%EB%B0%95%EC%88%98%20(10.18%2C%20%EC%97%B0%EC%8A%B5%EB%AC%B8%EC%A0%9C%2C%20Level_1).ipynb
+# index와 %연산자를 잘 이용한 예
+
+
