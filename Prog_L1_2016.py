b4362a4f84ce7ebf08b2313a1d735865252292db
Jae Min Kim <jamsince1994@naver.com>
2020-07-11 22:43:19 +0900

    Update Prog_L1_2016.py

--- Prog_L1_2016.py
+++ Prog_L1_2016.py
@@ -12,6 +12,18 @@ def solution(a, b):
 print(solution(5, 24))
 
 
+#다른 풀이
+def getDayName(a,b):
+    months = [31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+    days = ['FRI', 'SAT', 'SUN', 'MON', 'TUE', 'WED', 'THU']
+    return days[(sum(months[:a-1])+b-1)%7]
+
+#아래 코드는 테스트를 위한 출력 코드입니다.
+print(getDayName(5,24))
+
+
+
+#[max_month[month] for month in range(a-1)] 하.... months[:a-1]
 
 
 
