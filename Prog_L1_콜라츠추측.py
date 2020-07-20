cc8e674ccba84c94c6efd010ec4c6e4eee9fd717
Jae Min Kim <jamsince1994@naver.com>
2020-07-21 00:04:17 +0900

    Update Prog_L1_콜라츠추측.py

--- Prog_L1_콜라츠추측.py
+++ Prog_L1_콜라츠추측.py
@@ -9,20 +9,32 @@ def solution(num):
         c=count
         if num==1:
             break
-        num = [num/2,num*3+1][int(num%2)]
+        num = [num/2,num*3+1][int(num%2)] #짝수일때는 리스트의 0인덱스를, 홀수일때는 1인덱스.
     return c if num==1 else -1
     
     
 # 다른 사람 풀이는 하드코딩
+# num 은 1이상  8000000 미만이므로 1일때 1번으로 결과가 나오는 오류가 있다.
+def collatz(num):
+    for i in range(500):
+        num = num / 2 if num % 2 == 0 else num*3 + 1
+        if num == 1:
+            return i + 1
+    return -1
+
+# 아래는 테스트로 출력해 보기 위한 코드입니다.
+print(collatz(6))
+
 
 # 참고해서 수정한 내코드
 def solution(num):
     for count in range(500):
         if num==1:
-            return count+1
+            return count
         num = [num/2,num*3+1][int(num%2)]
     return -1
+# num==1 조건문을 앞에 둬서 1일때 오류가 없다
     
-# num 은 1이상 8000이하이므로 1일때 1번으로 결과가 나오는 오류가 있다.
+
     
     
