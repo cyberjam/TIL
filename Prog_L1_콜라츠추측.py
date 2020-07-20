af7c250e3c28a7ea84e524d7018c8f3738efe275
Jae Min Kim <jamsince1994@naver.com>
2020-07-20 23:56:02 +0900

    Create Prog_L1_콜라츠추측.py

---
+++ Prog_L1_콜라츠추측.py
@@ -0,0 +1,28 @@
+#1-1. 입력된 수가 짝수라면 2로 나눕니다. 
+#1-2. 입력된 수가 홀수라면 3을 곱하고 1을 더합니다.
+#2. 결과로 나온 수에 같은 작업을 1이 될 때까지 반복합니다.
+# 입력된 수가 6이라면 6→3→10→5→16→8→4→2→1 8번
+
+def solution(num):
+    c = 0
+    for count in range(500):
+        c=count
+        if num==1:
+            break
+        num = [num/2,num*3+1][int(num%2)]
+    return c if num==1 else -1
+    
+    
+# 다른 사람 풀이는 하드코딩
+
+# 참고해서 수정한 내코드
+def solution(num):
+    for count in range(500):
+        if num==1:
+            return count+1
+        num = [num/2,num*3+1][int(num%2)]
+    return -1
+    
+# num 은 1이상 8000이하이므로 1일때 1번으로 결과가 나오는 오류가 있다.
+    
+    
