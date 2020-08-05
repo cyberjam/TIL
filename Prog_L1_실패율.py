d1c35d220eaead437337317462717911f02fb2b5
Jae Min Kim <jamsince1994@naver.com>
2020-08-05 14:23:32 +0900

    Update Prog_L1_실패율.py

--- Prog_L1_실패율.py
+++ Prog_L1_실패율.py
@@ -19,3 +19,24 @@ def solution(N, stages):
     # idx 오름차순 정렬후 값 내림차순정렬
 
     return [idx[1] for idx in answer_sort]  #idx만 추출
+
+
+# 다른 사람 풀이
+# dictionary 는 순서가 없기 때문에 
+# 정렬이 되지 않는다는 생각을 가지고 dictionary를 사용하지 않았다
+# 그러나 sorted(dict)는 sort(dict.keys())와 같은 것이고
+# lambda를 사용하여 sorted(dict, key= lambda x : dict[x]) 는 
+# value를 기준으로 정렬하고 key를 반환하는 것이다.
+
+
+def solution(N, stages):
+    result = {}
+    denominator = len(stages)
+    for stage in range(1, N+1):
+        if denominator != 0:
+            count = stages.count(stage)
+            result[stage] = count / denominator
+            denominator -= count
+        else:
+            result[stage] = 0
+    return sorted(result, key=lambda x : result[x], reverse=True)
